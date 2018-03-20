b6ca54992ac1d9d0805a01dd49b8a71b934d8caa
root <root@sealab-10.242.138.114.cts.com>
2018-03-20 12:13:45 +0530

    Test
    Change-Id: I278a64a741512c52f10ffaaa52843a7d813fc7e0

---
+++ src/org/acme/pythonPipeline.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/groovy
+package org.acme;
+
+pythonPipeline(pipelineDefinition) {
+  // Create a globally accessible variable that makes
+  // the YAML pipeline definition available to all scripts
+  pd = pipelineDefinition
+}
+
+def executePipeline() {
+  node {
+    if (runTests) {
+      stage('Run Tests') {
+        //sh pd.testCommand
+	sh "Running the testcases"
+      }
+    }
+
+    if (deployUponTestSuccess) {
+      stage('Deploy') {
+        //sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+	sh "deploying the application"
+      }
+    }
+  }
+}
+
+return this
