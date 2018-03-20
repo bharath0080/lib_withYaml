c5ce407142e35bd0af3000a2c68dcd0e244f32f5
Name <you@example.com>
2018-03-20 08:20:36 -0700

    test

---
+++ src/org/acme/nodeJSPipeline.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/groovy
+package org.acme;
+
+nodeJSPipeline(pipelineDefinition) {
+  // Create a globally accessible variable that makes
+  // the YAML pipeline definition available to all scripts
+  pd = pipelineDefinition
+}
+
+def executePipeline() {
+  node('linux') {
+    if (runTests) {
+      stage('Run Tests') {
+        //sh pd.testCommand
+	sh "Running the testcases"
+      }
+    }
+
+    if (deployUponTestSuccess) {
+      stage('Deploy') {
+        //sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+	sh "deploying the application"
+      }
+    }
+  }
+}
+
+return this
