eb4bd68d63eb4c0f43e816b2f9a8ce66ee89596c
bharath0080 <bharathforany@gmail.com>
2018-03-20 20:53:13 +0530

    Update stdPipeline.groovy

--- src/org/acme/stdPipeline.groovy
+++ src/org/acme/stdPipeline.groovy
@@ -2,7 +2,7 @@
 package org.acme;
 import groovy.util.*
 //@Grab('org.yaml:snakeyaml:1.17')
-@Grab(group="org.yaml", module="snakeyaml", version="1.20")
+//@Grab(group="org.yaml", module="snakeyaml", version="1.20")
 
 
 
