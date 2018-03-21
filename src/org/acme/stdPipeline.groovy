40aa0daab962be607802ee58c6034701e90fe82e
bharath0080 <bharathforany@gmail.com>
2018-03-21 12:22:35 +0530

    Update stdPipeline.groovy

--- src/org/acme/stdPipeline.groovy
+++ src/org/acme/stdPipeline.groovy
@@ -15,10 +15,10 @@ def execute() {
       Yaml parser = new Yaml()
       Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yaml').text)
       println pipelineDefinition
-      println pipelineDefinition.pipelineType
+      typePipeline=pipelineDefinition.pipelineType
     
 
-		switch(pipelineDefinition.pipelineType) {
+		switch(typePipeline) {
 		case 'python':
 			// Instantiate and execute a Python pipeline
 			new pythonPipeline(pipelineDefinition).executePipeline()
