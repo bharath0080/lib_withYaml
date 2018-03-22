218f0c4bc6ca0d708294ab05e45d703683314545
bharath0080 <bharathforany@gmail.com>
2018-03-22 13:40:07 +0530

    Update pythonPipeline.groovy

--- src/org/acme/pythonPipeline.groovy
+++ src/org/acme/pythonPipeline.groovy
@@ -1,26 +1,11 @@
 #!/usr/bin/groovy
 package org.acme;
 import org.yaml.snakeyaml.*
-def executePipeline (){
- node {
-      echo "OK"
-      Yaml parser = new Yaml()
-      Map pipelineDefinition1 = parser.load(new File(pwd() + '/pipeline.yaml').text)  
-    //println pipelineDefinition
-    if (pipelineDefinition1.runTests) {
-      stage('Run Tests') {
-        //sh pd.testCommand
-	sh "echo Running the testcases"
-      }
-    }
-
-    if (pipelineDefinition1.deployUponTestSuccess) {
-      stage('Deploy') {
-        //sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
-	sh "echo deploying the application"
-      }
-    }
-  }
+def executeTestCases (pipelineDefinition){
+ 	sh "Running testcases"
+	sh pipelineDefinition.testCommand
 }
-
+def executeDeployment(){
+	sh "Deploying"
+}
 return this
