0ab4f8434636b3cfed1065adc9a113c47b5cbc98
bharath0080 <bharathforany@gmail.com>
2018-03-22 13:19:35 +0530

    Update pythonPipeline.groovy

--- src/org/acme/pythonPipeline.groovy
+++ src/org/acme/pythonPipeline.groovy
@@ -10,16 +10,16 @@ def executePipeline (pipelineDefinition){
   println deployOnTestSuccess
   node {
       Yaml parser = new Yaml()
-      Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yaml').text)  
+      Map pipelineDefinition1 = parser.load(new File(pwd() + '/pipeline.yaml').text)  
     //println pipelineDefinition
-    if (pipelineDefinition.runTests) {
+    if (pipelineDefinition1.runTests) {
       stage('Run Tests') {
         //sh pd.testCommand
 	sh "echo Running the testcases"
       }
     }
 
-    if (pipelineDefinition.deployUponTestSuccess) {
+    if (pipelineDefinition1.deployUponTestSuccess) {
       stage('Deploy') {
         //sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
 	sh "echo deploying the application"
