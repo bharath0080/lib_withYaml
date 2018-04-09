8fe97c352804ae20392119fc8e078183c7ca265b
root <root@sealab-10.242.138.114.cts.com>
2018-04-09 11:30:58 +0530

    test
    Change-Id: I9d2fd821189c8fd4d5997468e11b7a7b5c32bc6f

---
+++ src/org/acme/test.groovy
@@ -0,0 +1,15 @@
+#!/bin/groovy
+package org.acme;
+import groovy.util.*
+def execute() {
+	stage('clone'){
+		new testing().clone()
+	}
+	stage('build'){
+                new testing().build()
+
+	}
+	stage('Results'){
+		new testing().results()
+	}
+}
---
+++ src/org/acme/testing.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/groovy
+
+def clone(){
+	git 'https://github.com/bharath0080/SampleStudentProject.git'
+}
+def build(){
+	sh "'mvn' -Dmaven.test.failure.ignore clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true -Dsonar.jacoco.reportMissing.force.zero=True -Dsonar.junit.reportsPath=target/surefire-reports org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -Dsonar.host.url=http://10.242.138.71:9000 -Dsonar.login=admin -Dsonar.password=admin"
+}
+def results(){
+	jacoco maximumLineCoverage: '80', minimumLineCoverage: '80'
+}
