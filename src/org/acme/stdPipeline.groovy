c70342975f114bffb42602d0e66c90ff3acabde3
bharath0080 <bharathforany@gmail.com>
2018-04-09 12:22:45 +0530

    Update stdPipeline.groovy

--- src/org/acme/stdPipeline.groovy
+++ src/org/acme/stdPipeline.groovy
@@ -2,35 +2,16 @@
 package org.acme;
 import groovy.util.*
 def execute() {
-
-  node {
-
-    stage('Checkout') {
-      checkout scm
-    }
-	echo 'Loading pipeline definition'
-   config = readYaml file: "./pipeline.yaml"
-   println config
-   if(config.pipelineType == "python"){
-		println "in python"
-			if(config.runTests){
-				echo "In if"
-				stage('Run testcases'){
-					new pythonPipeline().executeTestCases(config)
-				}
-			}
-			if(config.deployUponTestSuccess){
-		        
-				echo "In another if"
-				stage('Deployment'){
-					
-					new pythonPipeline().executeDeployment(config)
-					echo "kumar"
-				}
-			}
-	}else if (config.pipelineType == "nodejs"){
-			echo "In Nodejs"
-	
+	node{
+	stage('clone'){
+		new testing().clone()
 	}
-  }
+	stage('build'){
+                new testing().build()
+
+	}
+	stage('Results'){
+		new testing().results()
+	}
+	}
 }
